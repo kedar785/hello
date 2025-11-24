c4f4f049008992088c8769b86276d4256ff29a23
kedar785 <baiskedar126@gmail.com>
2025-11-24 19:34:41 +0530

    str rotation

---
+++ New_practice/string_qus/chack_str_anothstr_rotation.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+int isRotation(char s1[],char s2[]){
+    if(strlen(s1) !=strlen(s2)){
+        return 0;
+    }
+    char temp[200];
+    strcpy(temp,s1);
+    strcat(temp,s1);
+
+    if(strstr (temp,s2) !=NULL){
+        return 1;
+    } else{
+        return 0;
+    }
+}
+int main(){
+    char s1[100],s2[100];
+    printf("Enter first string: ");
+    scanf("%s",s1);
+    printf("Enter second string: ");
+    scanf("%s",s2);
+    if(isRotation(s1,s2)){
+        printf("no it is a rotation. \n");
+    }
+    else{
+          printf("no it is not a rotation. \n");
+    
+    }
+    return 0;
+}
