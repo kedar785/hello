776f830abe686716ced41e5a26397765f0b2946b
kedar785 <baiskedar126@gmail.com>
2026-03-05 14:03:54 +0530

    Add files via upload

---
+++ overriding.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal makes sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main() {
+
+    Animal* obj;
+    Dog d;
+    obj = &d;
+
+    obj->sound();
+
+    return 0;
+}
